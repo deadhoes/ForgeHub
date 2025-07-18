70059bd782afc0ddf7cfa5d7eeeccbc7d1b8272d
EyeUI <noctalis.softworks@gmail.com>
2025-07-18 16:32:39 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,24 +1,18 @@
-local Discord = "https://discord.gg/9krxH9g4MH"
+# ForgeHub Loader
 
-print("Thanks for using ForgeHub.")
-print("Dont forget to join our discord! ".. Discord)
+**ForgeHub** is a free and open-source game hub for **Roblox**, designed to enhance your gameplay experience with supported games.
 
-local GameScripts = {
-    [124180759222403] = "https://raw.githubusercontent.com/deadhoes/ForgeHub/main/scripts/ants",
-    [73727760725654] = "https://raw.githubusercontent.com/deadhoes/ForgeHub/main/scripts/stealacapybara"
-	[75794995434025] = "https://raw.githubusercontent.com/deadhoes/ForgeHub/refs/heads/main/scripts/growabusiness"
-}
+## Loader
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/deadhoes/ForgeHub/refs/heads/main/loader.lua"))()
+```
 
-local currentGame = game.PlaceId
-local scriptUrl = GameScripts[currentGame]
 
-if scriptUrl then
-    local success, err = pcall(function()
-        loadstring(game:HttpGet(scriptUrl, true))()
-    end)
-    if not success then
-        warn("Script Error: "..tostring(err))
-    end
-else
-    warn("No script for this game (ID: "..currentGame)
-end
+
+| Supported Games  |
+| :------------ |
+|  Grow a bussines |
+|  Ants |
+|  Steal a Capybara |
+
+
